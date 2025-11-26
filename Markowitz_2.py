13df2568f181bf85914a021f2a4a288d1231f852
yungyaoc <yycheng910@gmail.com>
2025-11-26 22:49:01 +0800

    Part 2 done

--- Markowitz_2.py
+++ Markowitz_2.py
@@ -70,14 +70,67 @@ class MyPortfolio:
         """
         TODO: Complete Task 4 Below
         """
-        
-        
+
+        top_k = 3  # Each time select top k momentum sectors
+        defensive = ["XLP", "XLU", "XLV"]  # defensive sector
+
+        for current_date in self.price.index:
+            hist = self._get_historical_window(current_date, assets)
+            if hist is None:
+                continue
+
+            hist_ret = hist.pct_change().dropna()
+            if hist_ret.empty:
+                continue
+
+            candidates = self._select_candidates(hist, hist_ret, assets, top_k, defensive)
+            if not candidates:
+                continue
+
+            weights = self._calculate_candidate_weights(hist_ret, candidates)
+            self._assign_weights(current_date, candidates, weights)
+
         """
         TODO: Complete Task 4 Above
         """
 
         self.portfolio_weights.ffill(inplace=True)
         self.portfolio_weights.fillna(0, inplace=True)
+
+    def _get_historical_window(self, current_date, assets):
+        """Get historical price window for the lookback period."""
+        hist = self.price.loc[:current_date, assets]
+        if len(hist) < self.lookback:
+            return None
+        return hist.iloc[-self.lookback:]
+
+    def _select_candidates(self, hist, hist_ret, assets, top_k, defensive):
+        """Select candidate assets based on momentum strategy."""
+        momentum = hist.iloc[-1] / hist.iloc[0] - 1.0
+        pos_mom = momentum[momentum > 0].sort_values(ascending=False)
+
+        if len(pos_mom) == 0:
+            # If all are declining, switch to defensive sectors
+            return [a for a in defensive if a in assets]
+        else:
+            return list(pos_mom.index[:top_k])
+
+    def _calculate_candidate_weights(self, hist_ret, candidates):
+        """Calculate weights for candidates using inverse volatility."""
+        vol = hist_ret[candidates].std()
+        inv_vol = 1.0 / vol.replace(0, np.nan)
+
+        if inv_vol.isna().all():
+            return pd.Series(1.0 / len(candidates), index=candidates)
+
+        inv_vol = inv_vol.fillna(inv_vol.mean()).clip(lower=1e-6)
+        return inv_vol / inv_vol.sum()
+
+    def _assign_weights(self, current_date, candidates, weights):
+        """Assign calculated weights to portfolio for given date."""
+        self.portfolio_weights.loc[current_date, :] = 0.0
+        self.portfolio_weights.loc[current_date, candidates] = weights.values
+        self.portfolio_weights.loc[current_date, self.exclude] = 0.0
 
     def calculate_portfolio_returns(self):
         # Ensure weights are calculated
